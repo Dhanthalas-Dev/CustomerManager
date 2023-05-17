b46afa3ea7e9910def81b870f20e73334da02d75
Daniel Martel <marteld@inorca.es>
2023-05-17 15:07:22 +0100

    Added JsonStringEnumConverter to controllers

--- CustomerManagerWebApi/Program.cs
+++ CustomerManagerWebApi/Program.cs
@@ -5,11 +5,16 @@ using CustomerManagerRepositories.Repositories;
 using CustomerManagerWebApi.Extensions;
 using MediatR;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlite<ApplicationDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => 
+    { 
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); 
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
